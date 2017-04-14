05a750cf91abfb504cadf273b52be2a064d2e2a5
Gururaj Sridhar <gururaj.sridhar@autodesk.com>
2017-04-14 14:59:15 -0400

    added camera action and image view

--- app/src/main/java/guru/wishingcamera/MainScreen.java
+++ app/src/main/java/guru/wishingcamera/MainScreen.java
@@ -1,13 +1,47 @@
 package guru.wishingcamera;
 
-import android.support.v4.app.FragmentActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.provider.MediaStore;
+import android.graphics.Bitmap;
 
-public class MainScreen extends FragmentActivity {
+public class MainScreen extends Activity {
+
+    static final int REQUEST_IMAGE_CAPTURE=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+        Button startCamera = (Button) findViewById(R.id.camera_action);
+        startCamera.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
     }
+
+
+    protected void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ImageView imageCaptured = (ImageView) findViewById(R.id.image);
+            imageCaptured.setImageBitmap(imageBitmap);
+        }
+    }
+
 }
