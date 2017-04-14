07dc1832b4a701ac9b0d9a3b4ee73ec72c73181d
Gururaj Sridhar <gururaj.sridhar@autodesk.com>
2017-04-14 00:56:47 -0400

    Initial commit

---
+++ app/src/main/java/guru/wishingcamera/MainScreen.java
@@ -0,0 +1,13 @@
+package guru.wishingcamera;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+public class MainScreen extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+    }
+}
