6c284578fe45590eff55d60949f09d1669967345
Gururaj Sridhar <gururaj.sridhar@autodesk.com>
2017-04-15 11:51:56 -0400

    getting file path of temp file

--- app/src/main/java/guru/wishingcamera/MainScreen.java
+++ app/src/main/java/guru/wishingcamera/MainScreen.java
@@ -2,38 +2,67 @@ package guru.wishingcamera;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.provider.MediaStore;
-import android.graphics.Bitmap;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MainScreen extends Activity {
 
-    static final int REQUEST_IMAGE_CAPTURE=1;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
 
+        //Camera button
         Button startCamera = (Button) findViewById(R.id.camera_action);
         startCamera.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {
-                dispatchTakePictureIntent();
+                takePictureIntent();
             }
         });
+
+        Button savePhoto = (Button) findViewById(R.id.save);
+        savePhoto.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                saveEditedPhoto();
+            }
+        });
+
+
     }
 
+    //save the edited photo
+    protected void saveEditedPhoto() {
+        try {
+            File photoStorageDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+            File tempImage = File.createTempFile("temp", ".jpg", pho
+                    toStorageDirectory);
+            System.out.println(tempImage.getAbsolutePath());
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
 
-    protected void dispatchTakePictureIntent() {
+    //intent for the camera action
+    protected void takePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
     }
 
+    //this is the return method after the camera action is completed
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
