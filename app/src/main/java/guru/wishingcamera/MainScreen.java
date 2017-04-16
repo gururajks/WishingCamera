61b4b89e9e4cdfc972363c654b5c19b037baa5d7
Gururaj Sridhar <gururaj.sridhar@autodesk.com>
2017-04-16 00:58:36 -0400

    add text to the image that is captured by the camera

--- app/src/main/java/guru/wishingcamera/MainScreen.java
+++ app/src/main/java/guru/wishingcamera/MainScreen.java
@@ -3,19 +3,28 @@ package guru.wishingcamera;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainScreen extends Activity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    String tempFilePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,8 +45,6 @@ public class MainScreen extends Activity {
                 saveEditedPhoto();
             }
         });
-
-
     }
 
     //save the edited photo
@@ -45,7 +52,6 @@ public class MainScreen extends Activity {
         try {
             File photoStorageDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
             File tempImage = File.createTempFile("temp", ".jpg", photoStorageDirectory);
-            System.out.println(tempImage.getAbsolutePath());
         }
         catch (IOException e)
         {
@@ -57,18 +63,61 @@ public class MainScreen extends Activity {
     protected void takePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            //create the temp file
+            File tempPhotoFile = null;
+            try {
+                tempPhotoFile = createTempFile();
+            }
+            catch (IOException e)
+            {
+                System.out.println("Error in the SD Cards");
+            }
+            if (tempPhotoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "guru.wishingcamera.fileprovider",
+                        tempPhotoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+
         }
+    }
+
+    //create a temp file to read into the app
+    private File createTempFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+        tempFilePath = image.getAbsolutePath();
+        System.out.println("tempfile path:" + tempFilePath);
+        return image;
     }
 
     //this is the return method after the camera action is completed
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            //get the captured temp file
+            Bitmap capturedBitmap = BitmapFactory.decodeFile(tempFilePath);
+            //create a mutable image so that it can be edited
+            Bitmap mutableBitmap = capturedBitmap.copy(Bitmap.Config.ARGB_8888, true);
+
+            //create a canvas and edit the file
+            Canvas canvas = new Canvas(mutableBitmap);
+            Paint paint = new Paint();
+            paint.setColor(Color.WHITE);
+            paint.setTextSize(30);
+            canvas.drawText("Congrats", 20, 20, paint);
+            //set the image view with the edited file
             ImageView imageCaptured = (ImageView) findViewById(R.id.image);
-            imageCaptured.setImageBitmap(imageBitmap);
+            imageCaptured.setImageBitmap(mutableBitmap);
+
         }
     }
 
